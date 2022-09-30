1c13973dee1c52d489fb10a1694392874d85abce
kaolugo <kaolugo@umich.edu>
2022-09-30 17:14:19 -0400

    first commit

---
+++ src/ProvidedHomePage.jsx
@@ -0,0 +1,30 @@
+import logo from './levvel-logo.svg';
+import './App.css';
+import Sample from './Sample'
+
+function ProvidedHomePage() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+    
+            <div>
+              <Sample />
+            </div>
+          </header>
+        </div>
+      );
+}
+
+export default ProvidedHomePage;
---
+++ src/Sample.js
@@ -0,0 +1,9 @@
+function Sample() {
+    return (
+        <div>
+            This is a sample component !
+        </div>
+    );
+}
+
+export default Sample;
