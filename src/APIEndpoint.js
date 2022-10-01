9c5175958e69b61197448291e82e757891cee060
kaolugo <kaolugo@umich.edu>
2022-10-01 01:19:52 -0400

    add bullet point

--- src/APIEndpoint.js
+++ src/APIEndpoint.js
@@ -1,1 +1,3 @@
 export const USERS = 'https://jsonplaceholder.typicode.com/users';
+export const POSTS = 'https://jsonplaceholder.typicode.com/posts';
+export const USERPOSTS = 'https://jsonplaceholder.typicode.com/posts?userId=';
--- src/AuthorCard.jsx
+++ src/AuthorCard.jsx
@@ -1,12 +1,43 @@
 // import './AuthorCard.css';
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { USERPOSTS } from './APIEndpoint';
+
 function AuthorCard(props) {
     const info = props.info;
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get(USERPOSTS + info.id);
+                setData(response.data);
+                setError(null);
+            } catch (err) {
+                setError(err.message);
+                setData(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getData();
+    }, [info]);
+
+    console.log(data);
 
     return (
         <div class='card'>
             <div class='fullname'>{info.name}</div>
             <div class='username'>@{info.username}</div>
+            {data && <li class='postTitle'>{data[0].title}</li>}
+            {data && <div class='postBody'>{data[0].body}</div> }
+            {data && <li class='postTitle'>{data[1].title}</li>}
+            {data && <div class='postBody'>{data[1].body}</div> }
         </div>
     );
 }
