9015988c6b42d22ecd0a17475d0ca1338f09328f
kaolugo <kaolugo@umich.edu>
2022-10-01 09:48:13 -0400

    finished designing cards

--- src/AuthorCard.jsx
+++ src/AuthorCard.jsx
@@ -34,10 +34,10 @@ function AuthorCard(props) {
         <div class='card'>
             <div class='fullname'>{info.name}</div>
             <div class='username'>@{info.username}</div>
-            {data && <li class='postTitle'>{data[0].title}</li>}
-            {data && <div class='postBody'>{data[0].body}</div> }
-            {data && <li class='postTitle'>{data[1].title}</li>}
-            {data && <div class='postBody'>{data[1].body}</div> }
+            {data && <li class='postTitle'>  {data[0].title}</li>}
+            {data && <div class='postBody'>  {data[0].body}</div> }
+            {data && <li class='postTitle'>  {data[1].title}</li>}
+            {data && <div class='postBody'>  {data[1].body}</div> }
         </div>
     );
 }
